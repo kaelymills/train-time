3dae763b06efa7b8bad765ed7af6f9274bc9fad0
Kaelynne Mills <kaelymills@kaelynnes-mbp.valenciacc.edu>
2016-08-10 20:06:33 -0400

    Initial Heroku Deploy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,3 +6,36 @@
     storageBucket: "train-times-4fc9a.appspot.com",
   };
   firebase.initializeApp(config);
+
+  // Create a variable to reference the database.
+	var database = firebase.database()
+
+	// Initial Values
+	var trainName = "";
+	var destination = "";
+	var time = 0;
+	var frequency = "";
+
+
+// Capture Button Click
+$("#addUser").on("click", function() {
+
+	// Grabbed values from text boxes
+	trainName = $('#nameinput').val().trim();
+	destination = $('#roleinput').val().trim();
+	time = $('#startinput').val().trim();
+	frequency = $('#rateinput').val().trim();
+
+
+	// Code for handling the push
+	database.ref().push({
+		name: name,
+		destination: destination,
+		// startDate: startDate,
+		time: time,
+		frequency, frequency
+	})
+
+	// Don't refresh the page!
+	return false;
+});
