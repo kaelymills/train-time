c53f8b7b07ffc27db3223bafc682d88e981c60e6
Kaely Mills <kaelymills@knights.ucf.edu>
2016-08-16 22:32:03 -0400

    firebase works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,3 +39,24 @@ $("#addUser").on("click", function() {
 	// Don't refresh the page!
 	return false;
 });
+
+//Firebase data to print in 'Current Employees Table'
+	database.ref().on("child_added", function(childSnapshot) {
+		console.log(childSnapshot.val().name);
+		console.log(childSnapshot.val().destination);
+		console.log(childSnapshot.val().time);
+		console.log(childSnapshot.val().frequency);
+		// console.log(childSnapshot.val().st);
+
+		var tRow = $("<tr/>");
+		var nameDiv = $("<td/>").html(childSnapshot.val().name);
+		var roleDiv = $("<td/>").html(childSnapshot.val().destination);
+		var theDate = childSnapshot.val().startDate;
+		var dateDiv = $("<td/>").html(moment(theDate).format("MM/DD/YY"));
+		console.log(dateDiv);
+		var monthsDiv = $("<td/>").html(moment().diff(theDate, "months"));
+		tRow.append(nameDiv).append(roleDiv).append(dateDiv).append(monthsDiv);
+		$("#currentEmployees").append(tRow)
+
+	})
+
